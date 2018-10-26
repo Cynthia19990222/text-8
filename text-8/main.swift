23647514ac0b493d7b0176c175efd6ac3ae4f500
Cynthia19990222 <zxcccc987@qq.com>
2018-10-26 19:43:46 +0800

    Initial Commit

---
+++ text-8/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  text-8
+//
+//  Created by s20171103197 on 2018/10/26.
+//  Copyright © 2018 s20171103197. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
